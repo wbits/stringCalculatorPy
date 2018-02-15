0807138b7052b20794c0a3dbcb219599d929fe78
Dick Brouwers <dick@insided.nl>
2018-02-16 00:41:42 +0100

    string calculator

---
+++ string_calculator.py
@@ -0,0 +1,70 @@
+import unittest
+import re
+
+
+class StringCalculator:
+    def __init__(self, numbers):
+        self.numbers = numbers
+        self.result = 0
+
+    @staticmethod
+    def add(string):
+        calculator = StringCalculator(StringCalculator.split(string))
+
+        return calculator.get_result()
+
+    @staticmethod
+    def to_int(string):
+        try:
+            number = int(string)
+            if number < 0:
+                raise NegativeNumbersNotAllowed
+
+            return number
+        except ValueError:
+            return 0
+
+    @staticmethod
+    def split(string):
+        delimiter = ','
+        if string.find('//') == 0:
+            delimiter = string[2]
+
+        return re.split(delimiter + '|\n', string)
+
+    def get_result(self):
+        for i in range(0, len(self.numbers)):
+            self.result += self.to_int(self.numbers[i])
+
+        return self.result
+
+
+class NegativeNumbersNotAllowed(Exception):
+    pass
+
+
+class StringCalculatorTest(unittest.TestCase):
+    def assertRaisesWithMessage(self, msg, func, *args, **kwargs):
+        try:
+            func(*args, **kwargs)
+            self.assertFail()
+        except Exception as inst:
+            self.assertEqual(inst.message, msg)
+
+    def test_it_returns_zero_when_passing_an_empty_string(self):
+        self.assertEqual(0, StringCalculator.add(''))
+
+    def test_it_returns_an_integer_representing_the_value_of_the_string(self):
+        self.assertEqual(1, StringCalculator.add('1'))
+
+    def test_it_adds_all_numerical_values_separated_by_a_comma(self):
+        self.assertEqual(3, StringCalculator.add('1,2'))
+
+    def test_it_will_split_the_string_on_new_lines(self):
+        self.assertEqual(6, StringCalculator.add('1,2\n3'))
+
+    def test_it_allows_a_custom_delimiter(self):
+        self.assertEqual(10, StringCalculator.add('//:\n1:2:3:4'))
+
+    def test_it_will_raise_an_exception_when_the_input_contains_a_negative_number(self):
+        self.assertRaises(NegativeNumbersNotAllowed, StringCalculator.add, '-1')
